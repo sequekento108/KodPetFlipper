0a5d9b44a4e5aa917b437d25234be0bee81800c0
Devjow <contacto@devjow.net>
2022-12-26 13:38:23 +0000

    format and feed only heal if life is down

--- KodPet.c
+++ KodPet.c
@@ -5,25 +5,24 @@
 //  Created by KodamaDeveloper
 //
 
+#include "system/kodpet.h"
+#include "system/savedata.c"
+#include "system/savedata.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 #include <unistd.h>
-#include <string.h>
-#include "system/kodpet.h"
-#include "system/savedata.h"
-#include "system/savedata.c"
 
-//Flipper
+// Flipper
 
 //#include <furi.h>
 //#include <gui/gui.h>
 //#include <input/input.h>
 
-
 // Initialize a new Tamagotchi with default values
-Tamagotchi* createTamagotchi() {
-  Tamagotchi* t = malloc(sizeof(Tamagotchi));
+Tamagotchi *createTamagotchi() {
+  Tamagotchi *t = malloc(sizeof(Tamagotchi));
   t->status = HUNGRY;
   t->hunger = 50;
   t->happiness = 50;
@@ -37,86 +36,83 @@ Tamagotchi* createTamagotchi() {
   t->critic = 0;
   t->defend = 1;
   t->life = 100;
-      if(savedataControl(t, "n"))
-       return t;
+  if (savedataControl(t, "n"))
+    return t;
   return t;
 }
 
-
 // Perform an action on the Tamagotchi
-void doAction(Tamagotchi* t, int action) {
+void doAction(Tamagotchi *t, int action) {
   switch (action) {
-    case FEED:
-      t->hunger += 13;
-      t->life++;
-      if (t->hunger > 100) {
-        t->hunger = 100;
-      }
-      t->care--;
-      printf("########  LAST ACTION: FEED\n");
-      break;
-    case MEDICINE:
-      if (t->status == SICK) {
-        t->status = HAPPY;
-        t->hunger = 30;
-        t->happiness = 30;
-      }
-      printf("########  LAST ACTION: MEDICINE\n");
-      break;
-    case PLAY:
-      t->happiness += 12;
-      if (t->happiness > 100) {
-        t->happiness = 100;
-        t->status = HAPPY;
-      }
-      t->care--;
-      printf("########  LAST ACTION: PLAY\n");
-      break;
-    case CARE:
-      t->care += 10;
-      if (t->care > 30) {
-        t->care = 30;
-        t->happiness += HAPPY_FOR_CARE;
-      }
-      printf("########  LAST ACTION: CARE\n");
-      break;
+  case FEED:
+    t->hunger += 13;
+    if(t->life <= 99)
+     t->life++;
+    if (t->hunger > 100) {
+      t->hunger = 100;
+    }
+    t->care--;
+    printf("########  LAST ACTION: FEED\n");
+    break;
+  case MEDICINE:
+    if (t->status == SICK) {
+      t->status = HAPPY;
+      t->hunger = 30;
+      t->happiness = 30;
+    }
+    printf("########  LAST ACTION: MEDICINE\n");
+    break;
+  case PLAY:
+    t->happiness += 12;
+    if (t->happiness > 100) {
+      t->happiness = 100;
+      t->status = HAPPY;
+    }
+    t->care--;
+    printf("########  LAST ACTION: PLAY\n");
+    break;
+  case CARE:
+    t->care += 10;
+    if (t->care > 30) {
+      t->care = 30;
+      t->happiness += HAPPY_FOR_CARE;
+    }
+    printf("########  LAST ACTION: CARE\n");
+    break;
   }
 
-  if(t->status == DIRTY){
+  if (t->status == DIRTY) {
     t->happiness--;
     t->life--;
   }
-  
 }
 
 // Update the Tamagotchi's status based on its current values
-void updateStatus(Tamagotchi* t) {
+void updateStatus(Tamagotchi *t) {
   if (t->hunger <= DEAD_HUNGER || t->happiness <= DEAD_HAPPINESS) {
-    if(t->life >= 1){
+    if (t->life >= 1) {
       t->life -= DAMAGE_FOR_CARE;
-      if(t->life >=1)
-       return;
+      if (t->life >= 1)
+        return;
     }
     t->status = DEAD;
     return;
   } else if (t->hunger < 20 || t->happiness < 20) {
     t->status = SICK;
-  } else if(t->happiness >= 70){
+  } else if (t->happiness >= 70) {
     t->status = HAPPY;
-  }
-  else if(t->hunger < 50){
+  } else if (t->hunger < 50) {
     t->status = HUNGRY;
-  }else
-  {
+  } else {
     t->status = NORMAL;
   }
 
-  if(t->care <= 0){
+  if (t->care <= 0) {
     t->status = DIRTY;
-    t->life--;   
+    t->life--;
   }
-  
-  if(t->cycle >= CYCLE_EVOLUTION && t->evolution != QUANTUM){
+
+  if (t->cycle >= CYCLE_EVOLUTION && t->evolution != QUANTUM) {
     t->evolution++;
     t->cycle = 0;
     t->level++;
@@ -124,69 +120,68 @@ void updateStatus(Tamagotchi* t) {
   }
 
   if (t->evolution == QUANTUM) {
-     t->level++;
-     t->life = 100;
+    t->level++;
+    t->life = 100;
   }
-
 }
 
 // Print the current status of the Tamagotchi
-void printStatus(Tamagotchi* t) {
+void printStatus(Tamagotchi *t) {
   int selectionoptionuser = 0;
 
   printf("########\n");
   printf("Hunger: %d\n", t->hunger);
   printf("Happiness: %d\n", t->happiness);
   switch (t->status) {
-    case HUNGRY:
-      printf("Status: Hungry\n");
-      break;
-    case SICK:
-      printf("Status: Sick\n");
-      break;
-    case HAPPY:
-      printf("Status: HAPPY\n");
-      break;
-    case POWERED:
-      printf("Status: POWERED\n");
-      break;
-    case GOD:
-      printf("Status: GOD\n");
-      break;
-    case NORMAL:
-      printf("Status: NORMAL\n");
-      break;
-    case DIRTY:
-      printf("Status: DIRTY\n");
-      break;
-    case DEAD:
-      printf("Status: Dead\n");
-      break;
-   }
-    printf("Cycle: %i\n", t->cycle);
-    printf("Evolution: %i\n", t->evolution);
-    printf("Level: %i\n", t->level);
-    printf("Intelligence: %i\n", t->intelligence);
-    printf("Strength: %i\n", t->strength);
-    printf("Agility: %i\n", t->agility);
-    printf("Critic: %i\n", t->critic);
-    printf("Defend: %i\n", t->defend);
-    printf("Life: %i\n", t->life);
-    printf("\n\n######## Select Option: \n 0. FEED. \n 1. MEDICINE \n 2. PLAY \n 4. CARE \n 9. Random Cycle Time \n 99. Close test \n");
-    scanf("%d",&selectionoptionuser);
+  case HUNGRY:
+    printf("Status: Hungry\n");
+    break;
+  case SICK:
+    printf("Status: Sick\n");
+    break;
+  case HAPPY:
+    printf("Status: HAPPY\n");
+    break;
+  case POWERED:
+    printf("Status: POWERED\n");
+    break;
+  case GOD:
+    printf("Status: GOD\n");
+    break;
+  case NORMAL:
+    printf("Status: NORMAL\n");
+    break;
+  case DIRTY:
+    printf("Status: DIRTY\n");
+    break;
+  case DEAD:
+    printf("Status: Dead\n");
+    break;
+  }
+  printf("Cycle: %i\n", t->cycle);
+  printf("Evolution: %i\n", t->evolution);
+  printf("Level: %i\n", t->level);
+  printf("Intelligence: %i\n", t->intelligence);
+  printf("Strength: %i\n", t->strength);
+  printf("Agility: %i\n", t->agility);
+  printf("Critic: %i\n", t->critic);
+  printf("Defend: %i\n", t->defend);
+  printf("Life: %i\n", t->life);
+  printf("\n\n######## Select Option: \n 0. FEED. \n 1. MEDICINE \n 2. PLAY \n "
+         "4. CARE \n 9. Random Cycle Time \n 99. Close test \n");
+  scanf("%d", &selectionoptionuser);
 
-     int action = rand() % 3;
+  int action = rand() % 3;
   switch (selectionoptionuser) {
-    case 9:
-
-     doAction(t, action);
-     break;
-    case 99:
-     exit(0);
-     break;
-    default:
-     doAction(t, selectionoptionuser);
-     break;
+  case 9:
+    doAction(t, action);
+    break;
+  case 99:
+    exit(0);
+    break;
+  default:
+    doAction(t, selectionoptionuser);
+    break;
   }
 
   printf("########   Selected: %i\n", selectionoptionuser);
@@ -195,35 +190,29 @@ void printStatus(Tamagotchi* t) {
 int main() {
   srand(time(NULL));
 
-  Tamagotchi* t = createTamagotchi();
+  Tamagotchi *t = createTamagotchi();
 
   // Main game loop
-    while (t->status != DEAD) {
-      // Randomly choose an action for the Tamagotchi to perform
+  while (t->status != DEAD) {
+    // Randomly choose an action for the Tamagotchi to perform
 
+    // Decrease hunger and happiness over time
+    t->hunger -= CYCLE_HUNTER;
+    t->happiness -= CYCLE_HAPPINESS;
 
-      // Decrease hunger and happiness over time
-      t->hunger -= CYCLE_HUNTER;
-      t->happiness -= CYCLE_HAPPINESS;
+    // Update the Tamagotchi's status based on its current values
+    updateStatus(t);
 
-      // Update the Tamagotchi's status based on its current values
-      updateStatus(t);
+    // Print the current status of the Tamagotchi
+    printStatus(t);
+    t->cycle++;
 
-      // Print the current status of the Tamagotchi
-      printStatus(t);
-        t->cycle++;
-
-      if(savedataControl(t, "s") == 0)
-       printf("Saved progress");
-    }
-
-    // Game over
-    printf("Game over!\n");
-
-    return 0;
+    if (savedataControl(t, "s") == 0)
+      printf("Saved progress");
   }
 
+  // Game over
+  printf("Game over!\n");
 
-
-
-
+  return 0;
+}
--- system/savedata.c
+++ system/savedata.c
@@ -1,233 +1,209 @@
-  void dataopen(){
-    char ch, *fname;
-	FILE *fpts, *fptt;
-	
-	printf("\n\n Decrypt a text file :\n");
-	printf("--------------------------\n"); 	
-	
+void dataopen() {
+  char ch, *fname;
+  FILE *fpts, *fptt;
+
+  printf("\n\n Decrypt a text file :\n");
+  printf("--------------------------\n");
 
   fname = "memorycard/data.txt";
-	
-	fpts=fopen(fname, "w");
-	if(fpts==NULL)
-	{
-		printf(" File does not exists or error in opening..!!");
-		exit(7);
-	}
-	fptt=fopen("memorycard/temp.txt", "r");
-	if(fptt==NULL)
-	{
-		printf(" File does not exists or error in opening..!!");
-		fclose(fpts);
-		exit(9);
-	}
-	while(1)
-	{
-		ch=fgetc(fptt);
-		if(ch==EOF)
-		{
-			break;
-		}
-		else
-		{
-			ch=ch-100;
-			fputc(ch, fpts);
-		}
-	}
-	printf(" The file %s desprotected successfully..!!\n\n",fname);
-	fclose(fpts);
-	fclose(fptt);
-  }
-  void dataclose(){
-    	char *fname, ch;
-	FILE *fpts, *fptt;
-	
-	printf("\n\n Encrypt a text file :\n");
-	printf("--------------------------\n"); 	
-	
 
-	//scanf("%s",fname);	
+  fpts = fopen(fname, "w");
+  if (fpts == NULL) {
+    printf(" File does not exists or error in opening..!!");
+    exit(7);
+  }
+  fptt = fopen("memorycard/temp.txt", "r");
+  if (fptt == NULL) {
+    printf(" File does not exists or error in opening..!!");
+    fclose(fpts);
+    exit(9);
+  }
+  while (1) {
+    ch = fgetc(fptt);
+    if (ch == EOF) {
+      break;
+    } else {
+      ch = ch - 100;
+      fputc(ch, fpts);
+    }
+  }
+  printf(" The file %s desprotected successfully..!!\n\n", fname);
+  fclose(fpts);
+  fclose(fptt);
+}
+void dataclose() {
+  char *fname, ch;
+  FILE *fpts, *fptt;
+
+  printf("\n\n Encrypt a text file :\n");
+  printf("--------------------------\n");
+
+  // scanf("%s",fname);
   fname = "memorycard/data.txt";
 
-	fpts=fopen(fname, "r");
-	if(fpts==NULL)
-	{
-		printf(" File does not exists or error in opening..!!");
-		exit(1);
-	}
-	fptt=fopen("memorycard/temp.txt", "w");
-	if(fptt==NULL)
-	{
-		printf(" Error in creation of file temp.txt ..!!");
-		fclose(fpts);
-		exit(2);
-	}
-	while(1)
-	{
-		ch=fgetc(fpts);
-		if(ch==EOF)
-		{
-			break;
-		}
-		else
-		{
-			ch=ch+100;
-			fputc(ch, fptt);
-		}
-	}
-	fclose(fpts);
-	fclose(fptt);
-	fpts=fopen(fname, "w");
-	if(fpts==NULL)
-	{
-		printf(" File does not exists or error in opening..!!");
-		exit(3);
-	}
-	fptt=fopen("memorycard/temp.txt", "r");
-	if(fptt==NULL)
-	{
-		printf(" File does not exists or error in opening..!!");
-		fclose(fpts);
-		exit(4);
-	}
-	while(1)
-	{
-		ch=fgetc(fptt);
-		if(ch==EOF)
-		{
-			break;
-		}
-		else
-		{
-			fputc(ch, fpts);
-		}
-	}
-	printf(" File %s successfully protected ..!!\n\n", fname);
-	fclose(fpts);
-	fclose(fptt);
+  fpts = fopen(fname, "r");
+  if (fpts == NULL) {
+    printf(" File does not exists or error in opening..!!");
+    exit(1);
+  }
+  fptt = fopen("memorycard/temp.txt", "w");
+  if (fptt == NULL) {
+    printf(" Error in creation of file temp.txt ..!!");
+    fclose(fpts);
+    exit(2);
+  }
+  while (1) {
+    ch = fgetc(fpts);
+    if (ch == EOF) {
+      break;
+    } else {
+      ch = ch + 100;
+      fputc(ch, fptt);
+    }
+  }
+  fclose(fpts);
+  fclose(fptt);
+  fpts = fopen(fname, "w");
+  if (fpts == NULL) {
+    printf(" File does not exists or error in opening..!!");
+    exit(3);
+  }
+  fptt = fopen("memorycard/temp.txt", "r");
+  if (fptt == NULL) {
+    printf(" File does not exists or error in opening..!!");
+    fclose(fpts);
+    exit(4);
+  }
+  while (1) {
+    ch = fgetc(fptt);
+    if (ch == EOF) {
+      break;
+    } else {
+      fputc(ch, fpts);
+    }
+  }
+  printf(" File %s successfully protected ..!!\n\n", fname);
+  fclose(fpts);
+  fclose(fptt);
+}
+
+int parseInt(char *chars) {
+  int sum = 0;
+  int len = strlen(chars);
+  for (int x = 0; x < len; x++) {
+    int n = chars[len - (x + 1)] - '0';
+    sum = sum + powInt(n, x);
+  }
+  return sum;
+}
+
+int powInt(int x, int y) {
+  for (int i = 0; i < y; i++) {
+    x *= 10;
+  }
+  return x;
+}
+
+int savedataControl(Tamagotchi *t, char *mode) {
+  char *fileName = "memorycard/data.txt";
+  char *normal = "n";
+  char *save = "s";
+  if (!access(fileName, F_OK) && mode == normal) {
+    dataopen();
+    printf("The File %s\t was Found\n", fileName);
+    FILE *file_pointer;
+    char buffer[64], c;
+
+    file_pointer = fopen(fileName, "r");
+    printf("----read a line progress----\n");
+    fgets(buffer, 50, file_pointer);
+    printf("%s\n", buffer);
+
+    printf("----read and parse data progress----\n");
+    file_pointer = fopen(fileName, "r"); // reset the pointer
+    char str1[10], str2[10], str3[10], str4[10], str5[10], str6[10], str7[10],
+        str8[10], str9[10], str10[10], str11[10], str12[10], str13[10];
+    fscanf(file_pointer, "%s %s %s %s %s %s %s %s %s %s %s %s %s", str1, str2,
+           str3, str4, str5, str6, str7, str8, str9, str10, str11, str12,
+           str13);
+    printf("Read Data |%s|\n", str1);
+    printf("Read Data |%s|\n", str2);
+    printf("Read Data |%s|\n", str3);
+    printf("Read Data |%s|\n", str4);
+    printf("Read Data |%s|\n", str5);
+    printf("Read Data |%s|\n", str6);
+    printf("Read Data |%s|\n", str7);
+    printf("Read Data |%s|\n", str8);
+    printf("Read Data |%s|\n", str9);
+    printf("Read Data |%s|\n", str10);
+    printf("Read Data |%s|\n", str11);
+    printf("Read Data |%s|\n", str12);
+    printf("Read Data |%s|\n", str13);
+
+    printf("----read the entire file progress----\n");
+
+    file_pointer = fopen(fileName, "r"); // reset the pointer
+    while ((c = getc(file_pointer)) != EOF)
+      printf("%c", c);
+
+    t->status = parseInt(str1);
+    t->hunger = parseInt(str2);
+    t->happiness = parseInt(str3);
+    t->cycle = parseInt(str4);
+    t->care = parseInt(str5);
+    t->evolution = parseInt(str6);
+    t->level = parseInt(str7);
+    t->intelligence = parseInt(str8);
+    t->strength = parseInt(str9);
+    t->agility = parseInt(str10);
+    t->critic = parseInt(str11);
+    t->defend = parseInt(str12);
+    t->life = parseInt(str13);
+
+    fclose(file_pointer);
+    dataclose();
+    return 0;
+  } else {
+    if (mode != save) {
+      printf("The File %s\t not Found\n", fileName);
+    } else {
+      printf("Saved progress control");
+    }
+
+    int i;
+    FILE *fptr;
+    char fn[64];
+    char str[64];
+    snprintf(str, sizeof(str), "%i %i %i %i %i %i %i %i %i %i %i %i %i",
+             t->status, t->hunger, t->happiness, t->cycle, t->care,
+             t->evolution, t->level, t->intelligence, t->strength, t->agility,
+             t->critic, t->defend, t->life);
+    fptr = fopen(fileName, "w"); // "w" defines "writing mode"
+    for (i = 0; str[i]; i++) {
+      /* write to file using fputc() function */
+      fputc(str[i], fptr);
+    }
+    fclose(fptr);
+    dataclose();
+    return 0;
   }
 
-  int parseInt(char* chars)
-{
-    int sum = 0;
-    int len = strlen(chars);
-    for (int x = 0; x < len; x++)
-    {
-        int n = chars[len - (x + 1)] - '0';
-        sum = sum + powInt(n, x);
-    }
-    return sum;
-}
+  if (!access(fileName, R_OK)) {
+    printf("The File %s\t can be read\n", fileName);
+  } else {
+    printf("The File %s\t cannot be read\n", fileName);
+  }
 
-int powInt(int x, int y)
-{
-    for (int i = 0; i < y; i++)
-    {
-        x *= 10;
-    }
-    return x;
-}
+  if (!access(fileName, W_OK)) {
+    printf("The File %s\t it can be Edited\n", fileName);
+    return 1;
+  } else {
+    printf("The File %s\t it cannot be Edited\n", fileName);
+  }
 
-int savedataControl(Tamagotchi* t, char* mode){
- char *fileName = "memorycard/data.txt";
- char *normal = "n";
- char *save = "s";
-     if(!access(fileName, F_OK) && mode == normal){
-        dataopen();
-        printf("The File %s\t was Found\n",fileName);
-        FILE * file_pointer;
-        char buffer[64], c;
-
-        file_pointer = fopen(fileName, "r");
-        printf("----read a line progress----\n");
-        fgets(buffer, 50, file_pointer);
-        printf("%s\n", buffer);
-
-        printf("----read and parse data progress----\n");
-        file_pointer = fopen(fileName, "r"); //reset the pointer
-        char str1[10], str2[10], str3[10], str4[10], str5[10], str6[10], str7[10],str8[10],str9[10],str10[10],
-        str11[10],str12[10],str13[10];
-        fscanf(file_pointer, "%s %s %s %s %s %s %s %s %s %s %s %s %s", str1, str2, str3, str4, str5, str6, str7, str8, str9, str10, str11, str12, str13);
-        printf("Read Data |%s|\n", str1);
-        printf("Read Data |%s|\n", str2);
-        printf("Read Data |%s|\n", str3);
-        printf("Read Data |%s|\n", str4);
-        printf("Read Data |%s|\n", str5);
-        printf("Read Data |%s|\n", str6);
-        printf("Read Data |%s|\n", str7);
-        printf("Read Data |%s|\n", str8);
-        printf("Read Data |%s|\n", str9);
-        printf("Read Data |%s|\n", str10);
-        printf("Read Data |%s|\n", str11);
-        printf("Read Data |%s|\n", str12);
-        printf("Read Data |%s|\n", str13);
-
-        printf("----read the entire file progress----\n");
-
-        file_pointer = fopen(fileName, "r"); //reset the pointer
-        while ((c = getc(file_pointer)) != EOF) printf("%c", c);
-
-        t->status = parseInt(str1);
-        t->hunger = parseInt(str2);
-        t->happiness = parseInt(str3);
-        t->cycle = parseInt(str4);
-        t->care = parseInt(str5);
-        t->evolution = parseInt(str6);
-        t->level = parseInt(str7);
-        t->intelligence = parseInt(str8);
-        t->strength = parseInt(str9);
-        t->agility = parseInt(str10);
-        t->critic = parseInt(str11);
-        t->defend = parseInt(str12);
-        t->life = parseInt(str13);
-
-
-
-        fclose(file_pointer);
-        dataclose();
-        return 0;
-    }else{
-      if(mode != save){
-        printf("The File %s\t not Found\n",fileName);
-      }else
-      {
-        printf("Saved progress control");
-      }
-
-        int i;
-        FILE * fptr;
-        char fn[64];
-        char str[64];
-        snprintf(str, sizeof(str),"%i %i %i %i %i %i %i %i %i %i %i %i %i", t->status, t->hunger, t->happiness, t->cycle, t->care, t->evolution,
-        t->level, t->intelligence, t->strength, t->agility, t->critic, t->defend, t->life);
-        fptr = fopen(fileName, "w"); // "w" defines "writing mode"
-        for (i = 0; str[i]; i++) {
-            /* write to file using fputc() function */
-            fputc(str[i], fptr);
-        }
-        fclose(fptr);
-        dataclose();
-        return 0;
-    }
-
-    if(!access(fileName, R_OK )){
-        printf("The File %s\t can be read\n",fileName);
-    }else{
-        printf("The File %s\t cannot be read\n",fileName);
-
-    }
-
-    if(!access( fileName, W_OK )){
-        printf("The File %s\t it can be Edited\n",fileName);
-        return 1;
-    }else{
-        printf("The File %s\t it cannot be Edited\n",fileName);
-    }
-
-    if(!access( fileName, X_OK )){
-        printf("The File %s\t is an Executable\n",fileName);
-    }else{
-        printf("The File %s\t is not an Executable\n",fileName);
-    }
-
+  if (!access(fileName, X_OK)) {
+    printf("The File %s\t is an Executable\n", fileName);
+  } else {
+    printf("The File %s\t is not an Executable\n", fileName);
+  }
 }
