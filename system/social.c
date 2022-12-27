20a095e12a67627e13b8dcd90bc8f6a190d5c5be
Devjow <contacto@devjow.net>
2022-12-27 16:18:48 +0000

    -o social

--- system/social.c
+++ system/social.c
@@ -4,13 +4,13 @@
 
 void readsocial(char *npc) {
   if (npc == NULL) {
-    readfolder("enemies");
-    readfolder("friends");
-    readfolder("npc");
+    readfolder("enemies", 0);
+    readfolder("friends", 0);
+    readfolder("npc", 1);
   }
 }
 
-void readfolder(char* npc) {
+void readfolder(char *npc, int point) {
   DIR *folder;
   struct dirent *entry;
   int files = 0;
@@ -21,10 +21,23 @@ void readfolder(char* npc) {
 
   while ((entry = readdir(folder))) {
     files++;
-    if (!strstr(entry->d_name, ".")){
-      printf("%s %3d: %s\n", npc, files, entry->d_name);
-    }
+    if (point == 0)
+      if (!strstr(entry->d_name, ".")) {
+        printf("%s %3d: %s\n", npc, files, entry->d_name);
+      }
+    // npc
+    if (point == 1)
+      if (endswithfoonpc(entry->d_name) == 0) {
+        printf("%s %3d: %s\n", npc, files, entry->d_name);
+      }
   }
 
   closedir(folder);
+}
+
+int endswithfoonpc(char *string) {
+  string = strrchr(string, '.');
+  if (string != NULL)
+    return (strcmp(string, ".npcpet"));
+  return -1;
 }
--- system/social.h
+++ system/social.h
@@ -1,2 +1,3 @@
 void readsocial(char *npc);
-void readfolder(char* npc);
+void readfolder(char* npc, int point);
+int endswithfoonpc(char *string);
